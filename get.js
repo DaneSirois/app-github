a3755351eded3c20851a99aa5a1446066fdcb942
Dane Sirois <dane_sirois@live.com>
2016-10-10 18:47:15 +0000

    added get.js, and request.js scripts

---
+++ get.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var http = require('http');
+
+var cmdInput = process.argv[2];
+
+let readHTML = function (url, callback) {
+
+	const requestOptions = {
+		host: url,
+		path: "/"
+	};
+
+	http.get(requestOptions, (res) => {
+		
+		res.setEncoding("utf8");
+		var foo = "";
+
+		res.on("data", (data) => {
+			//console.log(data);	
+			foo += data;
+		});
+
+		res.on("end", () => {
+			callback(foo);
+		});
+
+
+	});
+
+};
+
+readHTML(cmdInput, (htmlData) => {
+	console.log(htmlData);
+});
---
+++ request.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+var cmdInput = process.argv[2];
+
+request(cmdInput, (err, res, body) => {
+
+	if (err) {
+		throw err;
+	}
+	
+	console.log(body);
+
+});
